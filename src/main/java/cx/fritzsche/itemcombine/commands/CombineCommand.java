4a16a3a2ecc4e7db085ad721a1b523313f12e695
BuildTools <unconfigured@null.spigotmc.org>
2024-06-02 18:35:17 +0200

    Allow combine command from console

--- src/main/java/cx/fritzsche/itemcombine/commands/CombineCommand.java
+++ src/main/java/cx/fritzsche/itemcombine/commands/CombineCommand.java
@@ -13,55 +13,53 @@ public class CombineCommand implements CommandExecutor {
     String prefix = "§9[ItemCombine]§7 ";
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("give")) {
-                    if (!player.hasPermission("itemcombine.give")) {
-                        player.sendMessage(prefix + "You don't have permission to use this command");
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("give")) {
+                if (!sender.hasPermission("itemcombine.give")) {
+                    sender.sendMessage(prefix + "You don't have permission to use this command");
+                    return false;
+                }
+
+                if (args.length > 2) {
+                    int amount = 1;
+                    if (args.length > 3 && args[3].matches("[0-9]+")) {
+                        amount = Integer.parseInt(args[3]);
+                    }
+
+                    ItemStack newItem = Items.createCombined(args[2], amount);
+                    if (newItem == null) {
+                        sender.sendMessage(prefix + "Item not found");
                         return false;
                     }
 
-                    if (args.length > 2) {
-                        int amount = 1;
-                        if (args.length > 3 && args[3].matches("[0-9]+")) {
-                            amount = Integer.parseInt(args[3]);
-                        }
-
-                        ItemStack newItem = Items.createCombined(args[2], amount);
-                        if (newItem == null) {
-                            player.sendMessage(prefix + "Item not found");
-                            return false;
-                        }
-
-                        Player targetPlayer = Bukkit.getPlayer(args[1]);
-                        if (targetPlayer != null) {
-                            targetPlayer.getInventory().addItem(newItem);
-                            player.sendMessage(prefix + "Item given to " + targetPlayer.getName());
-                        } else {
-                            player.sendMessage(prefix + "Player not found");
-                        }
+                    Player targetPlayer = Bukkit.getPlayer(args[1]);
+                    if (targetPlayer != null) {
+                        targetPlayer.getInventory().addItem(newItem);
+                        sender.sendMessage(prefix + "Item given to " + targetPlayer.getName());
                     } else {
-                        player.sendMessage(prefix + "Usage: /combine give <player> <item> (<amount>)");
+                        sender.sendMessage(prefix + "Player not found");
                     }
+                } else {
+                    sender.sendMessage(prefix + "Usage: /combine give <player> <item> (<amount>)");
                 }
-
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (!player.hasPermission("itemcombine.reload")) {
-                        player.sendMessage(prefix + "You don't have permission to use this command");
-                        return false;
-                    }
-
-                    player.sendMessage(prefix + "Reloading config");
-                    ItemCombine.plugin.reloadConfig();
-                    ItemCombine.plugin.reloadItemsConfig();
-                }
-            } else {
-                player.sendMessage(prefix + "§8ItemCombine 1.0");
-                player.sendMessage(prefix + "§7 /combine give <player> <item> (<amount>)");
-                player.sendMessage(prefix + "§7 /combine reload");
             }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("itemcombine.reload")) {
+                    sender.sendMessage(prefix + "You don't have permission to use this command");
+                    return false;
+                }
+
+                sender.sendMessage(prefix + "Reloading config");
+                ItemCombine.plugin.reloadConfig();
+                ItemCombine.plugin.reloadItemsConfig();
+            }
+        } else {
+            sender.sendMessage(prefix + "§8ItemCombine 1.0");
+            sender.sendMessage(prefix + "§7 /combine give <player> <item> (<amount>)");
+            sender.sendMessage(prefix + "§7 /combine reload");
         }
+
 
         return false;
     }
